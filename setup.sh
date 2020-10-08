d7efa2d6a01b42bd65081a7c27aded8ee599c14d
Markus <markus@greenrobot>
2020-10-08 14:44:52 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+generator_exe="objectbox-generator"
+if ! command -v $generator_exe &> /dev/null; then
+  generator_exe="./objectbox-generator"
+  if [ ! -f "$generator_exe" ]; then
+    generator_exe="../objectbox-generator/objectbox-generator"
+    if [ ! -f "$generator_exe" ]; then
+      echo "Please get the latest ObjectBox Generator release from here:"
+      echo "https://github.com/objectbox/objectbox-generator/releases/latest"
+      echo "More details: https://github.com/objectbox/objectbox-generator"
+      exit
+    fi
+  fi
+fi
+
+echo "$generator_exe found"
+"$generator_exe" -cpp ts-data-model.fbs
+
+rsync -a *.{hpp,cpp} "src/"
+
+lib_version="0.10.0"
+curl -s https://raw.githubusercontent.com/objectbox/objectbox-c/master/download.sh | bash /dev/stdin $lib_version
+ls -lhR download/
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "^^^ Please check if you need to update the includes from the download dir ^^^"
+
+# The standard lib in download/ does not come with TS, contact ObjectBox to get a TS lib
+lib_file="../objectbox/cmake-build-relwithdebinfo/objectbox-c/libobjectbox.so"
+if [ -f "$lib_file" ]; then
+  cp "$lib_file" lib/
+  ls -lh lib/
+fi
